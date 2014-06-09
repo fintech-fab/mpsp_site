bd2fd7aba81687745c5130b48d47fb7107cfde31
mikhailn <m.novikov@kreddy.ru>
2014-06-09 18:09:33 +0400

    example local config

---
+++ app/config/examples/api.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+	'url'      => 'http://custom.mpsp.url',
+	'currency' => 'EUR',
+);
---
+++ app/config/examples/app.php
@@ -0,0 +1,12 @@
+<?php
+return [
+
+	'debug'     => true,
+
+	'key'       => 'random-key',
+
+	'providers' => [
+		100500 => 'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider', // ide-helper
+	],
+
+];
